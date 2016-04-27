ac83c021e02ae4cef991e78d81c0872e70e962e4
jdely <jjdely@gmail.com>
2016-04-27 17:47:50 -0400

    second push

--- picap.sh
+++ picap.sh
@@ -4,9 +4,9 @@
 # Automatic packet capture for Raspberry Pi
 
 ## Path
-#rootdir=~/pitap
-#pcapdir=$root/captures
-#logFile=picap.log
+rootdir=~/picap
+pcapdir=$rootdir/captures
+logFile=$rootdir/picap.log
 int=eno16777736
 state="$(cat /sys/class/net/${int}/carrier)"
 runtime=10s
@@ -30,4 +30,4 @@ echo "${state}"
 
 sessionTimestamp=$(date +"session-%d-%m-%y-at-%H-%M")
 echo "Starting dump mode at $sessionTimestamp. Captures stored the base directory." >> $logFile
-timeout "${runtime}" tcpdump -i $int -w $sessionTimestamp-$RANDOM.pcapng
+timeout "${runtime}" tcpdump -i $int -w $picapdir/$sessionTimestamp-$RANDOM.pcapng
---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Initial PiCAP setup
+
+#Create Directories
+mkdir ~/picap
+mkdir ~/picap/captures
+cd ~/picap
+
+wget https://github.com/jdely/picap.sh
+
