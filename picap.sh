7f41c88c3e05156018b12e8b26e127c4eda3e9ee
jdely <jjdely@gmail.com>
2016-04-29 15:49:48 -0400

    version 1

--- picap.sh
+++ picap.sh
@@ -3,14 +3,18 @@
 # PiCAP
 # Automatic packet capture for Raspberry Pi
 
-## Path
-rootdir=~/picap
-pcapdir=$rootdir/captures
-logFile=$rootdir/picap.log
+## Path Variables
+picapdir=/home/pi/picap/captures
+logFile=/home/pi/picap/picap.log
+
+## Session Variables
 int=eth0
 state="$(cat /sys/class/net/${int}/carrier)"
 runtime=10s
-filecount="$(ls -l $pcapdir | wc -l)" 
+sessionNumber=$(($(ls -l $picapdir | wc -l)))
+
+## Log File
+exec 3>&1 1>>${logFile} 2>&1
 
 # Interface Prep
 ifconfig $int 0.0.0.0
@@ -19,7 +23,7 @@ ifconfig $int up
 for (( ; ; ))
 do
 
-	echo "${state}"
+	printf "Carrier Value: ${state}\n" | tee /dev/fd/3
 	state="$(cat /sys/class/net/${int}/carrier)"
 	if [ "${state}" = 1 ]
 	then
@@ -27,8 +31,17 @@ do
 	fi
 done
 
-echo "${state}"
+sessionTimestamp=$(date +"%d-%m-%y-at-%H-%M")
 
-sessionTimestamp=$(date +"session-%d-%m-%y-at-%H-%M")
-echo "Starting dump mode at $sessionTimestamp. Captures stored the base directory." >> $logFile
-timeout "${runtime}" tcpdump -i $int -w $picapdir/$sessionTimestamp-$RANDOM.pcapng
+printf "Ethernet Carrier Signal Detected.\n" | tee /dev/fd/3
+printf "Starting capture session at $sessionTimestamp.\n Captures stored at $picapdir\n" | tee /dev/fd/3
+printf "Capture File Created: capture-$sessionNumber-$sessionTimestamp-$RANDOM.pcapng\n" | tee /dev/fd/3
+
+timeout "${runtime}" tcpdump -i $int -w $picapdir/session-$sessionNumber-$sessionTimestamp-$RANDOM.pcapng
+
+printf "Session timeout reached.\n\n" | tee /dev/fd/3
+
+chmod 644 /home/pi/picap/picap.log
+chown pi:pi /home/pi/picap/picap.log
+chmod 644 /home/pi/picap/captures/*
+chown pi:pi /home/pi/picap/captures/*
